d633e0135dfd75e782d27d48a25ce156c8894b91
ShriGit01 <shripavanb@gmail.com>
2026-03-30 14:00:17 +0530

    Introducing JoinUs with two JDs and Seesion for CartItem

---
+++ Models/CartItem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShriFoodsProj.Models
+{
+    public class CartItemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential= true;
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
